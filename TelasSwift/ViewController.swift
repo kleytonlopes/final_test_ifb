704c4bc85f7c6f7f266bb63bfa05b09b756afab1
IFB-BIOTIC-16 <ifb-biotic-16@iMac-de-IFB-BIOTIC-16.local>
2023-12-08 09:31:27 -0300

    config: initial ios project

---
+++ TelasSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TelasSwift
+//
+//  Created by IFB-BIOTIC-16 on 08/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
